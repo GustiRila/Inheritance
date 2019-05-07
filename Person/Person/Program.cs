583a8e03959cef6321cd466f2c1d65eafe117285
GustiRila <praja.rila31@gmail.com>
2019-05-07 14:52:44 +0700

    Inheritance

---
+++ Person/Person/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mike", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Teacher", 35, "190302", "Math");
+            teacher.GetNameAndAge();
+
+            Students students = new Students("Sara", 19, "10506", "sara1234@amikom.ac.id");
+            students.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Person/Person/Students.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Students : Person
+    {
+        public string StudentsId { get; set; }
+        public string Email { get; set; }
+
+        public Students (string name, int age, string id, string subject) : base (name, age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
